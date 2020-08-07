d82f7e5dc4f32042292d72cbf6ab6c781f7d136d
Kyun <kyun.c137@gmail.com>
2020-08-07 08:29:57 +0800

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -15,19 +15,19 @@ esac
 BASE=${bin_abs_path}
 
 if [ "$1" == "base" ] ; then
-    docker build --no-cache -t canal/osbase $BASE/base
+    docker build --no-cache -t iamkyun/osbase $BASE/base
 elif [ "$1" == "base_v2" ] ; then
-    docker build --no-cache -t canal/osbase $BASE/base_v2 -f $BASE/base_v2/Dockerfile_v2
+    docker build --no-cache -t iamkyun/osbase $BASE/base_v2 -f $BASE/base_v2/Dockerfile_v2
 elif [ "$1" == "base_admin" ] ; then
-    docker build --no-cache -t canal/osadmin_admin $BASE/base_admin -f $BASE/base_admin/Dockerfile_admin
+    docker build --no-cache -t iamkyun/osadmin_admin $BASE/base_admin -f $BASE/base_admin/Dockerfile_admin
 elif [ "$1" == "admin" ] ; then
     rm -rf $BASE/canal.*.tar.gz ;
     cd $BASE/../ && mvn clean package -Dmaven.test.skip -Denv=release && cd $current_path ;
     cp $BASE/../target/canal.admin-*.tar.gz $BASE/
-    docker build --no-cache -t canal/canal-admin $BASE/ -f $BASE/Dockerfile_admin
+    docker build --no-cache -t iamkyun/canal-admin $BASE/ -f $BASE/Dockerfile_admin
 else 
     rm -rf $BASE/canal.*.tar.gz ; 
     cd $BASE/../ && mvn clean package -Dmaven.test.skip -Denv=release && cd $current_path ;
     cp $BASE/../target/canal.deployer-*.tar.gz $BASE/
-    docker build --no-cache -t canal/canal-server $BASE/
+    docker build --no-cache -t iamkyun/canal-server $BASE/
 fi
